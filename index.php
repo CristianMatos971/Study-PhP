4052c96266623d3b2790308a44f015424b621404
CristianMatos <cristianernandes971@gmail.com>
2025-10-06 15:15:55 -0400

    First test connection to PostgreSQL database

--- index.php
+++ index.php
@@ -6,10 +6,26 @@
 //include 'practice_challenges/job_listings_HelpFunctions.php';
 //include 'practice_challenges/Fahrenheit_to_Celsius.php';
 //include 'practice_challenges/find_longest_word.php';
-include 'practice_challenges/oop_challenges.php';
+//include 'practice_challenges/oop_challenges.php';
+//include 'practice_challenges'
 
 //$output = null;
+
+$host = "localhost";
+$port = "5432";
+$dbname = "first_php_project";  // substitua pelo nome que você criou no HeidiSQL
+$user = "postgres";
+$password = "postgres";   // a senha definida na instalação do PostgreSQL
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "✅ Conexão bem-sucedida com o PostgreSQL!";
+} catch (PDOException $e) {
+    echo "❌ Erro: " . $e->getMessage();
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,7 +41,8 @@ include 'practice_challenges/oop_challenges.php';
 <body style='background-color: rgba(33, 33, 34, 1); color: white; font-family: Arial, Helvetica, sans-serif;'-->
     <p>
         <strong>
-            <?php echo $output; ?>
+            <?php //echo $output; 
+            ?>
         </strong>
     </p>
 </body>
