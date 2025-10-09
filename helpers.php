816e72c86e8b4013969072e343fbf090e75cd737
CristianMatos <cristianernandes971@gmail.com>
2025-10-09 14:47:38 -0400

    Split the UI into partials and shorten some directories requirement through helper functions

--- helpers.php
+++ helpers.php
@@ -10,3 +10,34 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Encurtar a string pra carregar uma view
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        require "$viewPath";
+    } else {
+        echo "The view $name was not found!";
+    }
+}
+
+/**
+ * Encurtar a string pra carregar um partial
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (file_exists($partialPath)) {
+        require "$partialPath";
+    } else {
+        echo "The view $name was not found!";
+    }
+}
